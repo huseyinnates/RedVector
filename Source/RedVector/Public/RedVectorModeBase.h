d6a91d91e3039424b9c938d727f016837e948370
huseyinnates <alternativerealitystudiomain@gmail.com>
2025-04-11 00:06:23 +0300

    Switch: from visual studio to rider + riderlink added

---
+++ Source/RedVector/Public/RedVectorModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RedVectorModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDVECTOR_API ARedVectorModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
